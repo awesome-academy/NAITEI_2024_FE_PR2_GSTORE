d78cd65355b55dbbcab3c8aedef46f26a52b0998
Chu Đình Hiển <hienc1212@gmail.com>
2024-11-07 15:21:10 +0700

    Done footer

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -30,13 +30,13 @@ const UserMenu: React.FC<UserMenuProps> = ({ mobile }) => {
       >
         <FaHeart className='h-5 w-5 mb-1 hover:scale-110 transition-transform duration-300 hover:text-red-500' />
         <small className='text-xs text-gray-400 transition-colors duration-300 hover:text-red-500'>
-          {t('wish list')}
+          {t('wish_list')}
         </small>
       </Link>
       <Link to='/cart' className='flex flex-col items-center px-4 hover:border-gray-400 transition-all duration-300'>
         <FaShoppingCart className='h-5 w-5 mb-1 hover:scale-110 transition-transform duration-300 hover:text-yellow-400' />
         <small className='text-xs text-gray-400 transition-colors duration-300 hover:text-yellow-400'>
-          {t('my cart')}
+          {t('my_cart')}
         </small>
       </Link>
 
