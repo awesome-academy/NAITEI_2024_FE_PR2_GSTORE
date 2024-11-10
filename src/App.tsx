45d8b9744d1c848e71bfdefd0d8baadb0b8744aa
betosan2810 <vle80444@gmail.com>
2024-11-10 22:51:03 +0700

    tao db json + fetch product

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import Footer from './layouts/Footer'
-import Header from './layouts/Header'
+import { Routes, Route } from 'react-router-dom';
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
+import Home from './page/Home';
+import ProductList from './page/ProductList'; 
 
 function App() {
   return (
     <>
       <Header />
-      <br></br>
+      <main className="min-h-screen p-4">
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Route cho trang Home */}
+          <Route path="/products" element={<ProductList />} /> {/* Route cho trang ProductList */}
+        </Routes>
+      </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
