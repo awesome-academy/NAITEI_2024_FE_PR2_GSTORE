0caf25c7edef006b9e0b8942033c7bf67e424134
betosan2810 <vle80444@gmail.com>
2024-11-20 00:59:43 +0700

    product-detail + wishlist

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import './i18n/i18n.ts'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import './i18n/i18n.ts';
+import { BrowserRouter } from 'react-router-dom';
+import { WishlistProvider } from './components/WishlistContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <WishlistProvider>
+      <App />
+      <ToastContainer /> {/* Thêm ToastContainer */}
+    </WishlistProvider>
   </BrowserRouter>
-)
+);
